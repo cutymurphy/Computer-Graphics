5e6c255f2cec08439527c5dfbe6d32d1130fa584
Darya Sushkova <dasha.030105@gmail.com>
2023-11-12 22:43:12 +0300

    added some interfaces for upper classes..

---
+++ src/task2/CircleAlgorithm.java
@@ -0,0 +1,7 @@
+package task2;
+
+import java.util.List;
+
+public interface CircleAlgorithm {
+    List<Point> getPoints();
+}
---
+++ src/task2/SectorAlgorithm.java
@@ -0,0 +1,7 @@
+package task2;
+
+import java.util.List;
+
+public interface SectorAlgorithm extends CircleAlgorithm {
+    List<Point> getSectorPoints();
+}
