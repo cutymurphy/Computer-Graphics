a8f942be062e7e40a11b656f5b7d20e577e9fc44
Darya Sushkova <dasha.030105@gmail.com>
2023-12-11 00:21:02 +0300

    added tests module (JUnit5)

---
+++ src/Task3/ObjReaderInitial/tests/Task3/ObjReaderInitial/src/com/cgvsu/model/ModelTest.java
@@ -0,0 +1,16 @@
+package Task3.ObjReaderInitial.src.com.cgvsu.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ModelTest {
+
+    @Test
+    void calculateNormalsForVertexes() {
+    }
+
+    @Test
+    void calculateNormalsForPolygons() {
+    }
+}
