51e0716ce9f40668273843a9b43948a73d1203f8
Darya Sushkova <dasha.030105@gmail.com>
2023-12-11 00:18:33 +0300

    objreader logics

---
+++ src/Task3/ObjReaderInitial/src/com/cgvsu/objreader/ObjReaderException.java
@@ -0,0 +1,7 @@
+package Task3.ObjReaderInitial.src.com.cgvsu.objreader;
+
+public class ObjReaderException extends RuntimeException {
+    public ObjReaderException(String errorMessage, int lineInd) {
+        super("Error parsing OBJ file on line: " + lineInd + ". " + errorMessage);
+    }
+}
